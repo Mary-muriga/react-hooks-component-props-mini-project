a0a6a5472e2c319ceed27feba5560809a9e66c04
Marymuriga <marymuriga@gmail.com>
2022-07-16 13:28:24 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header"
+import About from "./About"
+import ArticleList from "./ArticleList"
 
-console.log(blogData);
+//console.log(blogData);
 
 function App() {
   return (
